b4df55e2eefff37e23cf9e84451efa2ba4ec023f
Tomas Gonzalez <tom.gonzalezc@duocuc.cl>
2025-11-25 12:29:20 -0300

    Integración con API externa, creación de archivo ExternalApi y modificación en catalogoViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
 
     val room_version = "2.6.1"
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -51,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -58,21 +60,26 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
     implementation("androidx.compose.material:material-icons-extended-android")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    //GPS
+    
     implementation("com.google.android.gms:play-services-location:21.2.0")
-    //Autenticación Biométrica
     implementation("androidx.biometric:biometric:1.1.0")
-    //Guardar credenciales de forma segura
     implementation("androidx.security:security-crypto:1.0.0")
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+    
     implementation(libs.coil.compose)
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    testImplementation("io.mockk:mockk:1.13.5") 
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 }
